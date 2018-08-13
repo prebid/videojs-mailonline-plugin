406d7ac9558843257a294200372a7e21d6f9ab55
Yevgeniy Nisman <ynisman@appnexus.com>
2018-08-13 13:21:15 -0700

    Version 1.2.7

--- src/scripts/videojs_5.vast.vpaid.js
+++ src/scripts/videojs_5.vast.vpaid.js
@@ -14,7 +14,7 @@ window._molSettings = null;
 require('./plugin/components/ads-label_5');
 require('./plugin/components/black-poster_5');
 
-console.log('Prebid MailOnline plugin version 1.2.5');
+console.log('Prebid MailOnline plugin version 1.2.7');
 
 var videoJsVAST = require('./plugin/videojs.vast.vpaid');
 
