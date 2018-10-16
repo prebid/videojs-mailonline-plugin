992c58a02b41fcabd3a4add0c6e0e6923c4d94ee
Yevgeniy Nisman <ynisman@appnexus.com>
2018-10-16 15:52:11 -0700

    Removed commented line

--- src/scripts/videojs_5.vast.vpaid.js
+++ src/scripts/videojs_5.vast.vpaid.js
@@ -11,7 +11,6 @@ var videojs = window.videojs || {
 
 window._molSettings = null;
 
-// require('./plugin/components/ads-label_5');
 require('./plugin/components/black-poster_5');
 
 console.log('Prebid MailOnline plugin version 1.2.9');
