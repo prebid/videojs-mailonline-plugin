3de2da11af73a727ef247561b823e1aea7dbc5dd
Yevgeniy Nisman <ynisman@appnexus.com>
2019-01-31 15:53:39 -0800

    MOL Plugin 1.3.10

--- src/scripts/videojs_5.vast.vpaid.js
+++ src/scripts/videojs_5.vast.vpaid.js
@@ -14,7 +14,7 @@ window._molSettings = null;
 require('./plugin/components/black-poster_5');
 var logger = require ('./utils/consoleLogger');
 
-logger.log('Prebid MailOnline plugin version 1.3.9');
+logger.log('Prebid MailOnline plugin version 1.3.10');
 
 var videoJsVAST = require('./plugin/videojs.vast.vpaid');
 
