49278b99823d8a8e1d9f947036feb4e57f45e310
Yevgeniy Nisman <ynisman@appnexus.com>
2020-04-22 13:30:20 -0700

    Fix for VSSPP-1350 (Brightcove Embedded Page with Prebid MOL Plugin - Ads with ad icons do not play)

--- src/scripts/videojs_5.vast.vpaid.js
+++ src/scripts/videojs_5.vast.vpaid.js
@@ -14,7 +14,7 @@ window._molSettings = null;
 require('./plugin/components/black-poster_5');
 var logger = require('./utils/consoleLogger');
 
-logger.log('Prebid MailOnline plugin version 1.3.16');
+logger.log('Prebid MailOnline plugin version 1.3.17');
 
 var videoJsVAST = require('./plugin/videojs.vast.vpaid');
 
